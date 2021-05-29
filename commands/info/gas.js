f5f39aee089b17b2add53c76f967aed5508d5933
Mateus Dyonisio <mateeusdyonisio@gmail.com>
2021-05-29 00:10:24 -0300

    Update Gas Script

--- commands/info/gas.js
+++ commands/info/gas.js
@@ -11,31 +11,20 @@ module.exports = {
     description: "Mostra o gás atual",
     run: async (client, message, args) => {
         const channel = message.channel
-        const url = 'https://etherscan.io/gastracker'
+        const url = `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.TOKEN_ETHERSCAN}`
         axios.get(url).then(res => {
-            const html = res.data;
-            var $ = cheerio.load(html);
-
-            const lowGasDiv = $('#divLowPrice > div > span > span')
-            const averageGasDiv = $('#divAvgPrice > span > span')
-            const highGasDiv = $('#divHighPrice > span > font > span')
-
-            const lowGasTimePrice = $('#divLowPrice > div.text-secondary').text()
-            const averageGasTimePrice = $('#divAvgPrice > div.text-secondary').text()
-            const highGasTimePrice = $('#divHighPrice > div.text-secondary').text()
-
             const emoji = message.member.guild.emojis.cache.find(emoji => emoji.name === 'eth')
 
             const embed = new MessageEmbed()
                 .setAuthor(message.member.user.tag, message.member.user.displayAvatarURL())
                 .setTitle(`${emoji} GAS ETH ${emoji}`)
                 .addFields(
-                    { name: '**:grin: Low**', value: `${lowGasDiv.text()} gwei \n${lowGasTimePrice}‎‎`, inline: true},
-                    { name: '**:grinning: Average**', value: `${averageGasDiv.text()} gwei \n${averageGasTimePrice}`, inline: true },
-                    { name: '**:slight_smile: High**', value: `${highGasDiv.text()} gwei \n${highGasTimePrice}`, inline: true }
+                    { name: '**:grin: Low**', value: `${res.data.result.SafeGasPrice} gwei`, inline: true},
+                    { name: '**:grinning: Average**', value: `${res.data.result.ProposeGasPrice} gwei`, inline: true },
+                    { name: '**:slight_smile: High**', value: `${res.data.result.FastGasPrice} gwei`, inline: true }
                 )
                 .setColor('#868DAC')
-                .setFooter(`${url} • Ultima atualização`)
+                .setFooter(`https://etherscan.io/gastracker • Ultima atualização`)
                 .setTimestamp()
 
             channel.send(embed)
