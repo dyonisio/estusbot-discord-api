ea377d9d0d21045811c489cdb72df3837cbd0e81
Mateus Dyonisio <mateeusdyonisio@gmail.com>
2021-05-29 01:11:46 -0300

    Updated Emojis

--- commands/info/recompensas.js
+++ commands/info/recompensas.js
@@ -8,10 +8,20 @@ module.exports = {
     run: async (client, message, args) => {
         const channel = message.channel
 
+        const e1 = message.member.guild.emojis.cache.find(emoji => emoji.name === '1')
+        const e2 = message.member.guild.emojis.cache.find(emoji => emoji.name === '2')
+        const e3 = message.member.guild.emojis.cache.find(emoji => emoji.name === '3')
+        const e4 = message.member.guild.emojis.cache.find(emoji => emoji.name === '4')
+        const e6 = message.member.guild.emojis.cache.find(emoji => emoji.name === '6_')
+        const slp = message.member.guild.emojis.cache.find(emoji => emoji.name === 'slp')
+        const axs = message.member.guild.emojis.cache.find(emoji => emoji.name === 'axs')
+        const stonks = message.member.guild.emojis.cache.find(emoji => emoji.name === 'stonks')
+
+
         channel.send(`
         :youtube: BENEFÍCIOS DO CLUBE :youtube:
 
-:1:     :2:     :3:     :4:     :6_:
+${e1}     ${e2}     ${e3}     ${e4}     ${e6}
 
 Nível 1: Inscrito Épico
 ― ― ―
@@ -32,8 +42,8 @@ Nível 2: Gamer Lendário
 Nível 3: Gamer Mítico
 ― ― ―
 :open_file_folder:  Recompensas dos níveis anteriores
-:slp:  Mentoria de Axie Infinity
-:axs:  E-book 101 Coisas Que Você Precisa Saber Sobre Axie Infinity
+${slp}  Mentoria de Axie Infinity
+${axs}  E-book 101 Coisas Que Você Precisa Saber Sobre Axie Infinity
 :briefcase:  Privilégio: Gamer Mítico
 :tickets:  Cartão NFT: "Gamer Mítico" (5 WAXP)
 ― ― ―
@@ -41,7 +51,7 @@ Nível 3: Gamer Mítico
 Nível 4: Treinador Prime
 ― ― ―
 :open_file_folder:  Recompensas dos níveis anteriores
-:stonks:  Garante 1 vaga para a Estus Academy (scholarship)
+${stonks}  Garante 1 vaga para a Estus Academy (scholarship)
 :briefcase:  Privilégio: Treinador Prime
 :tickets:  Cartão NFT: "Treinador Prime" (10 WAXP)
 ― ― ―
